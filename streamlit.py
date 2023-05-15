27a98fc8e64c2e32fdd83e3c5bf8eb49e172b79b
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 01:00:18 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,46 @@
 import streamlit as st
 import pandas as pd
-
+from streamlit.hashing import _CodeHasher
 import base64
+
 # Title of the web app
 st.title("Sentence Tagging App")
 
 # Load the dataframe
-data = pd.read_csv(r"morph_order_by_count_2023_01_15.csv")
+data = pd.read_csv("your_dataframe.csv")
 
-# Display the dataframe
-st.subheader("Original Dataset")
-st.dataframe(data)
+# Create a unique session ID for each user
+session_id = st.report_thread.get_report_ctx().session_id
+session_hash = _CodeHasher().hash(session_id)
+user_id = session_hash % (10 ** 8)
 
 # Create a copy of the dataframe to store the tagged values
 tagged_data = data.copy()
 
+# Check if user tags exist in the session state, initialize if not
+if 'user_tags' not in st.session_state:
+    st.session_state['user_tags'] = {}
+
 # Iterate over each row in the dataframe
 for index, row in tagged_data.iterrows():
-    sentence = row["Sentence"]
+    sentence = row["sentence"]
 
     # Display the sentence
     st.subheader(f"Sentence {index + 1}")
     st.write(sentence)
 
-    # # Create a selectbox for tagging options
-    selected_tag = st.selectbox \
-        ("Select a tag", ["a", "b", "c", "d", "e"], key = row,)
-    #
-    # # Store the selected tag in the tagged_data dataframe
-    tagged_data.at[index, "tag"] = selected_tag
+    # Create a selectbox for tagging options
+    selected_tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"])
 
-# Display the tagged dataframe
+    # Store the selected tag in the user_tags dictionary
+    st.session_state['user_tags'][(user_id, index)] = selected_tag
+
+# Display the tagged dataset
 st.subheader("Tagged Dataset")
+tagged_data['tag'] = [st.session_state['user_tags'].get((user_id, index)) for index in range(len(tagged_data))]
 st.dataframe(tagged_data)
 
-
+# Download the tagged dataset as a CSV file
 csv = tagged_data.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()
 href = f'<a href="data:file/csv;base64,{b64}" download="tagged_data.csv">Download CSV</a>'
