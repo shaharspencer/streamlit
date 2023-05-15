f981ed5759660b5cbb4bb34c21e826b6eaf3d579
shaharspencer <63598264+shaharspencer@users.noreply.github.com>
2023-05-16 00:39:28 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+# Title of the web app
+st.title("Dummy Data Streamlit App")
+
+# Create a dummy dataframe
+data = pd.DataFrame({
+    'Column 1': np.random.randn(100),
+    'Column 2': np.random.randint(0, 10, 100),
+    'Column 3': np.random.uniform(1.0, 5.0, 100)
+})
+
+# Display the dataset
+st.subheader("Dummy Dataset")
+st.dataframe(data)
+
+# Show descriptive statistics
+st.subheader("Descriptive Statistics")
+st.write(data.describe())
 
