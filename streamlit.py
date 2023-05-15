1dabf8925a675cacde3719b144363aae5db71e08
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 00:44:41 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -1,22 +1,33 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 
 # Title of the web app
-st.title("Dummy Data Streamlit App")
+st.title("Sentence Tagging App")
 
-# Create a dummy dataframe
-data = pd.DataFrame({
-    'Column 1': np.random.randn(100),
-    'Column 2': np.random.randint(0, 10, 100),
-    'Column 3': np.random.uniform(1.0, 5.0, 100)
-})
+# Load the dataframe
+data = pd.read_csv("C:\Users\User\OneDrive\Documents\CreativeLanguageOutputFiles\rarest_sentences_by_dim\morph_order_by_count_2023_01_15.csv")
 
-# Display the dataset
-st.subheader("Dummy Dataset")
+# Display the dataframe
+st.subheader("Original Dataset")
 st.dataframe(data)
 
-# Show descriptive statistics
-st.subheader("Descriptive Statistics")
-st.write(data.describe())
+# Create a copy of the dataframe to store the tagged values
+tagged_data = data.copy()
 
+# Iterate over each row in the dataframe
+for index, row in tagged_data.iterrows():
+    sentence = row["Sentence"]
+
+    # Display the sentence
+    st.subheader(f"Sentence {index + 1}")
+    st.write(sentence)
+
+    # Create a selectbox for tagging options
+    selected_tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"])
+
+    # Store the selected tag in the tagged_data dataframe
+    tagged_data.at[index, "tag"] = selected_tag
+
+# Display the tagged dataframe
+st.subheader("Tagged Dataset")
+st.dataframe(tagged_data)
