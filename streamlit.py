421b35e3331c04e4cb2154ebcb31ab7cfdcf75c9
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 01:17:45 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -30,23 +30,42 @@ def logout():
         st.session_state.user = None
         st.success("Logged out")
 
+# Function to tag sentences
+def tag_sentences(dataframe):
+    st.title("Tag Sentences")
+    st.write("Tag sentences with options 'a', 'b', 'c', 'd', 'e'")
+    for index, row in dataframe.iterrows():
+        sentence = row["sentence"]
+        st.write(f"**Sentence {index+1}:** {sentence}")
+        tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"])
+        # Perform any necessary processing or storage of the tag
+
 # Main app - User1 section
 def user1_page():
     st.title("User1 Page")
     st.write("Welcome to User1's section!")
-    # Add user-specific content for User1
+    # Load or generate a dataframe for User1
+    dataframe_user1 = pd.DataFrame({"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
+    # Call the tagging function for User1
+    tag_sentences(dataframe_user1)
 
 # Main app - User2 section
 def user2_page():
     st.title("User2 Page")
     st.write("Welcome to User2's section!")
-    # Add user-specific content for User2
+    # Load or generate a dataframe for User2
+    dataframe_user2 = pd.DataFrame({"sentence": ["Sentence A", "Sentence B", "Sentence C"]})
+    # Call the tagging function for User2
+    tag_sentences(dataframe_user2)
 
 # Main app - User3 section
 def user3_page():
     st.title("User3 Page")
     st.write("Welcome to User3's section!")
-    # Add user-specific content for User3
+    # Load or generate a dataframe for User3
+    dataframe_user3 = pd.DataFrame({"sentence": ["Example 1", "Example 2", "Example 3"]})
+    # Call the tagging function for User3
+    tag_sentences(dataframe_user3)
 
 # Main app
 def main():
