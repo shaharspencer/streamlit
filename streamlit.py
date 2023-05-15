e87d69c0f9920c48d26bd87152b8ac730d82e153
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 01:31:20 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+
 # Define the list of valid users
 valid_users = ["Shahar", "Gabi", "Ittamar", "Nurit"]
+
 
 # User authentication
 def authenticate(username):
@@ -12,33 +14,57 @@ def authenticate(username):
     else:
         return False
 
+
 # Login form
 def login():
-    username = st.selectbox("Select User", valid_users, format_func=lambda user: f"👤 {user}", key=0)
+    username = st.selectbox("Select User", valid_users,
+                            format_func=lambda user: f"👤 {user}",
+                            key="login_selectbox")
     if st.button("Login"):
         if authenticate(username):
             # Store the authenticated user in session state
             st.session_state.user = username
+            st.session_state.df_modified = False  # Flag to track if dataframe is modified
             st.success(f"Logged in as {username}")
         else:
             st.error("Invalid username")
 
+
 # Logout button
 def logout():
     if st.button("Logout"):
-        # Clear the session state user
+        # Clear the session state variables
         st.session_state.user = None
+        st.session_state.df_modified = False
+        st.session_state.df = None
         st.success("Logged out")
 
+
 # Function to tag sentences
-def tag_sentences(dataframe):
+def tag_sentences():
     st.title("Tag Sentences")
     st.write("Tag sentences with options 'a', 'b', 'c', 'd', 'e'")
-    for index, row in dataframe.iterrows():
+
+    # Retrieve the dataframe from session state or create a new one
+    if 'df' not in st.session_state or st.session_state.df is None:
+        st.session_state.df = pd.DataFrame(
+            {"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
+
+    # Iterate over the dataframe and allow users to tag sentences
+    for index, row in st.session_state.df.iterrows():
         sentence = row["sentence"]
-        st.write(f"**Sentence {index+1}:** {sentence}")
-        tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"], key=row)
-        # Perform any necessary processing or storage of the tag
+        st.write(f"**Sentence {index + 1}:** {sentence}")
+        tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"],
+                           key=f"tag_selectbox_{index}")
+        # Update the dataframe with the selected tag
+        st.session_state.df.at[index, "tag"] = tag
+        st.session_state.df_modified = True  # Set the modified flag
+
+    # Save changes to the dataframe if it has been modified
+    if st.session_state.df_modified:
+        st.session_state.df_modified = False  # Reset the modified flag
+        st.success("Changes saved!")
+
 
 # Main app
 def main():
@@ -52,27 +78,18 @@ def main():
         if user == "Shahar":
             st.title("Shahar's Page")
             st.write("Welcome to Shahar's section!")
-            dataframe_shahar = pd.DataFrame({"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
-            tag_sentences(dataframe_shahar)
+            tag_sentences()
         elif user == "Gabi":
             st.title("Gabi's Page")
             st.write("Welcome to Gabi's section!")
-            dataframe_gabi = pd.DataFrame({"sentence": ["Sentence A", "Sentence B", "Sentence C"]})
-            tag_sentences(dataframe_gabi)
+            tag_sentences()
         elif user == "Ittamar":
             st.title("Ittamar's Page")
             st.write("Welcome to Ittamar's section!")
-            dataframe_ittamar = pd.DataFrame({"sentence": ["Example 1", "Example 2", "Example 3"]})
-            tag_sentences(dataframe_ittamar)
+            tag_sentences()
         elif user == "Nurit":
             st.title("Nurit's Page")
             st.write("Welcome to Nurit's section!")
-            dataframe_nurit = pd.DataFrame({"sentence": ["Text 1", "Text 2", "Text 3"]})
-            tag_sentences(dataframe_nurit)
+            tag_sentences()
 
-        # Add a logout button in the sidebar
-        st.sidebar.button("Logout", on_click=logout)
-
-# Run the app
-if __name__ == '__main__':
-    main()
+        # Add a logout button in the
