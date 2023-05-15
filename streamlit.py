9182742f459f0a06c0c8e2de2703dccdfb543673
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 01:08:49 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import base64
-from session_state import SessionState
 
 # Title of the web app
 st.title("Sentence Tagging App")
@@ -10,14 +9,14 @@ st.title("Sentence Tagging App")
 data = pd.read_csv("morph_order_by_count_2023_01_15.csv")
 
 # Create a unique session ID for each user
-session_state = SessionState.get(user_id=0)
+session_id = st._get_report_id()
 
 # Create a copy of the dataframe to store the tagged values
 tagged_data = data.copy()
 
 # Check if user tags exist in the session state, initialize if not
-if not hasattr(session_state, 'user_tags'):
-    session_state.user_tags = {}
+if 'user_tags' not in st.session_state:
+    st.session_state['user_tags'] = {}
 
 # Iterate over each row in the dataframe
 for index, row in tagged_data.iterrows():
@@ -31,11 +30,18 @@ for index, row in tagged_data.iterrows():
     selected_tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"])
 
     # Generate a unique user ID based on session ID and index
-    user_id = f"{session_state.user_id}-{index}"
+    user_id = f"{session_id}-{index}"
 
     # Store the selected tag in the user_tags dictionary
-    session_state.user_tags[user_id] = selected_tag
+    st.session_state['user_tags'][user_id] = selected_tag
 
 # Display the tagged dataset
 st.subheader("Tagged Dataset")
-tagged
+tagged_data['tag'] = [st.session_state['user_tags'].get(f"{session_id}-{index}") for index in range(len(tagged_data))]
+st.dataframe(tagged_data)
+
+# Download the tagged dataset as a CSV file
+csv = tagged_data.to_csv(index=False)
+b64 = base64.b64encode(csv.encode()).decode()
+href = f'<a href="data:file/csv;base64,{b64}" download="tagged_data.csv">Download CSV</a>'
+st.markdown(href, unsafe_allow_html=True)
