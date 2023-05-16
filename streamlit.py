6ee5ae323745988e570f1dcdbc61feb43906a42f
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 20:44:56 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -12,7 +12,8 @@ def main():
 
     if annotation_button:
         # Display annotation page
-        show_annotation_page(selected_user)
+        with st.spinner():
+            show_annotation_page(selected_user)
 
 
 def show_annotation_page(user):
@@ -21,17 +22,30 @@ def show_annotation_page(user):
         st.session_state.df = pd.DataFrame(
             {"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
 
-        # Iterate over the dataframe and allow users to tag sentences
+    # Iterate over the dataframe and allow users to tag sentences
     for index, row in st.session_state.df.iterrows():
         sentence = row["sentence"]
         st.write(f"**Sentence {index + 1}:** {sentence}")
         tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"],
-                           key=f"tag_selectbox_{index}")
+                           key=f"tag_selectbox_{index}",
+                           index=get_current_tag_index(user, index))
         # Update the dataframe with the selected tag
-        st.session_state.df.at[index, "tag"] = tag
+        st.session_state.df.at[index, get_user_column(user)] = tag
 
     # Display annotation page for each row in the data file
 
+
+def get_current_tag_index(user, index):
+    column = get_user_column(user)
+    if column in st.session_state.df.columns:
+        tag = st.session_state.df.at[index, column]
+        if tag in ["a", "b", "c", "d", "e"]:
+            return ["a", "b", "c", "d", "e"].index(tag)
+    return 0
+
+
+def get_user_column(user):
+    return f"{user}_tag"
 
 
 if __name__ == "__main__":
