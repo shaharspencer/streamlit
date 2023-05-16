11e6612dbf5109917a076779fd4dd917793e6f33
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 20:56:05 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -1,62 +1,102 @@
 import streamlit as st
 import pandas as pd
 
-def main():
-    # Create sidebar with two sections
-    st.sidebar.title("Annotation App")
-    st.sidebar.header("Annotation Pages")
-    user_list = ["User 1", "User 2", "User 3"]  # List of users
-    selected_user = st.sidebar.selectbox("Select user", user_list)
+# Define the list of valid users
+valid_users = ["Shahar", "Gabi", "Ittamar", "Nurit"]
 
-    annotation_button = st.sidebar.button("Open Annotation Page")
+# User authentication
+def authenticate(username):
+    # Check if the provided username is in the list of valid users
+    if username in valid_users:
+        return True
+    else:
+        return False
 
-    if annotation_button:
-        # Display annotation page
-        show_annotation_page(selected_user)
+# Login form
+def login():
+    username = st.selectbox("Select User", valid_users,
+                            format_func=lambda user: f"👤 {user}",
+                            key="login_selectbox")
+    if st.button("Login"):
+        if authenticate(username):
+            # Store the authenticated user in session state
+            st.session_state.user = username
+            st.success(f"Logged in as {username}")
+        else:
+            st.error("Invalid username")
+
+# Logout button
+# Logout button
+# Logout button
+def logout():
+    if st.button("Logout"):
+        # Save changes to the dataframe if it has been modified
+        if st.session_state.df_modified:
+            st.session_state.df.to_csv("data.csv", index=False)
+            st.success("Changes saved!")
+        else:
+            st.info("No changes to save.")
+
+        # Clear the session state variables
+        st.session_state.user = None
+        st.session_state.df_modified = False
+        st.session_state.df = None
+        st.success("Logged out.")
 
 
-def show_annotation_page(user):
-    # Read the data file
+
+# Function to tag sentences
+def tag_sentences():
+    st.title("Tag Sentences")
+    st.write("Tag sentences with options 'a', 'b', 'c', 'd', 'e'")
+
+    # Retrieve the dataframe from session state or create a new one
     if 'df' not in st.session_state or st.session_state.df is None:
         st.session_state.df = pd.DataFrame(
             {"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
 
-    # Get or create the annotation data for the selected user
-    annotation_file = f"{user}_annotation.csv"
-    if annotation_file not in st.session_state:
-        if not st.session_state.df.empty:
-            annotation_data = pd.DataFrame(columns=["Sentence", "Annotation"])
-            annotation_data["Sentence"] = st.session_state.df["sentence"]
-            annotation_data.to_csv(annotation_file, index=False)
-        else:
-            annotation_data = pd.DataFrame()
-        st.session_state[annotation_file] = annotation_data
-    else:
-        annotation_data = st.session_state[annotation_file]
-
     # Iterate over the dataframe and allow users to tag sentences
     for index, row in st.session_state.df.iterrows():
         sentence = row["sentence"]
-        selected_option = 0
-        tag = st.selectbox("Select a tag", ["", "a", "b", "c", "d", "e"],
-                           index=selected_option,
-                           key=f"tag_selectbox_{user}_{index}")
-        # Update the annotation data for the current user
-        update_annotation_data(annotation_data, sentence, tag)
-        annotation_data.to_csv(annotation_file, index=False)
+        st.write(f"**Sentence {index + 1}:** {sentence}")
+        tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"],
+                           key=f"tag_selectbox_{index}")
+        # Update the dataframe with the selected tag
+        st.session_state.df.at[index, "tag"] = tag
 
-def get_selected_option(annotation_data, sentence):
-    annotation_row = annotation_data.loc[annotation_data["Sentence"] == sentence]
-    if not annotation_row.empty:
-        return annotation_row["Annotation"].values[0]
-    return 0
+    # Add a save button to save changes to the dataframe
+    if st.button("Save Changes"):
+        st.session_state.df.to_csv("data.csv", index=False)
+        st.success("Changes saved!")
 
-def update_annotation_data(annotation_data, sentence, tag):
-    annotation_row = annotation_data.loc[annotation_data["Sentence"] == sentence]
-    if not annotation_row.empty:
-        annotation_data.loc[annotation_row.index, "Annotation"] = tag
+# Main app
+def main():
+    st.title("Multiple User Streamlit App")
+
+    if 'user' not in st.session_state or st.session_state.user is None:
+        login()
     else:
-        annotation_data = annotation_data.append({"Sentence": sentence, "Annotation": tag}, ignore_index=True)
+        user = st.session_state.user
+
+        if user == "Shahar":
+            st.title("Shahar's Page")
+            st.write("Welcome to Shahar's section!")
+            tag_sentences()
+        elif user == "Gabi":
+            st.title("Gabi's Page")
+            st.write("Welcome to Gabi's section!")
+            tag_sentences()
+        elif user == "Ittamar":
+            st.title("Ittamar's Page")
+            st.write("Welcome to Ittamar's section!")
+            tag_sentences()
+        elif user == "Nurit":
+            st.title("Nurit's Page")
+            st.write("Welcome to Nurit's section!")
+            tag_sentences()
+
+        # Add a logout button
+        logout()
 
 if __name__ == "__main__":
     main()
