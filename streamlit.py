daed9ebb434d808073e7631b04475a78abc4136e
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 20:41:58 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -17,36 +17,21 @@ def main():
 
 def show_annotation_page(user):
     # Read the data file
-    data_file = "your_dataframe.csv"
-    data = pd.read_csv(data_file)
+    if 'df' not in st.session_state or st.session_state.df is None:
+        st.session_state.df = pd.DataFrame(
+            {"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
 
-    # Get or create the annotation data for the selected user
-    annotation_file = f"{user}_annotation.csv"
-    if "annotation_data" not in st.session_state:
-        st.session_state.annotation_data = {}
-
-    if annotation_file not in st.session_state.annotation_data:
-        annotation_data = pd.DataFrame(columns=["Sentence", "Annotation"])
-        st.session_state.annotation_data[annotation_file] = annotation_data
-    else:
-        annotation_data = st.session_state.annotation_data[annotation_file]
+        # Iterate over the dataframe and allow users to tag sentences
+    for index, row in st.session_state.df.iterrows():
+        sentence = row["sentence"]
+        st.write(f"**Sentence {index + 1}:** {sentence}")
+        tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"],
+                           key=f"tag_selectbox_{index}")
+        # Update the dataframe with the selected tag
+        st.session_state.df.at[index, "tag"] = tag
 
     # Display annotation page for each row in the data file
-    for index, row in data.iterrows():
 
-
-        for index, row in st.session_state.df.iterrows():
-            sentence = row["sentence"]
-            st.write(f"**Sentence {index + 1}:** {sentence}")
-            tag = st.selectbox("Select a tag", ["a", "b", "c", "d", "e"],
-                               key=f"tag_selectbox_{index}")
-            # Update the dataframe with the selected tag
-            st.session_state.df.at[index, "tag"] = tag
-
-        # # Update the session state with the updated annotation data
-        # st.session_state.annotation_data[annotation_file] = annotation_data
-        #
-        # st.write("---")  # Add a separator between rows
 
 
 if __name__ == "__main__":
