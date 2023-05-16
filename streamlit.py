73fc803eb6ddb43f750cc1a1e912ea83ec76b11b
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 20:49:35 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -22,34 +22,42 @@ def show_annotation_page(user):
             {"sentence": ["Sentence 1", "Sentence 2", "Sentence 3"]})
 
     # Get or create the annotation data for the selected user
-    if 'annotation_data' not in st.session_state:
-        st.session_state.annotation_data = {}
-    if user not in st.session_state.annotation_data:
-        annotation_data = [""] * len(st.session_state.df)
-        st.session_state.annotation_data[user] = annotation_data
+    annotation_file = f"{user}_annotation.csv"
+    if annotation_file not in st.session_state:
+        if not st.session_state.df.empty:
+            annotation_data = pd.DataFrame(columns=["Sentence", "Annotation"])
+            annotation_data["Sentence"] = st.session_state.df["sentence"]
+            annotation_data.to_csv(annotation_file, index=False)
+        else:
+            annotation_data = pd.DataFrame()
+        st.session_state[annotation_file] = annotation_data
     else:
-        annotation_data = st.session_state.annotation_data[user]
+        annotation_data = st.session_state[annotation_file]
 
     # Iterate over the dataframe and allow users to tag sentences
     for index, row in st.session_state.df.iterrows():
         sentence = row["sentence"]
         st.write(f"**Sentence {index + 1}:** {sentence}")
+        selected_option = get_selected_option(annotation_data, sentence)
         tag = st.selectbox("Select a tag", ["", "a", "b", "c", "d", "e"],
-                           key=f"tag_selectbox_{user}_{index}",
-                           index=get_current_tag_index(annotation_data, index))
+                           index=selected_option,
+                           key=f"tag_selectbox_{user}_{index}")
         # Update the annotation data for the current user
-        annotation_data[index] = tag
+        update_annotation_data(annotation_data, sentence, tag)
+        annotation_data.to_csv(annotation_file, index=False)
 
-    # Update the session state with the updated annotation data
-    st.session_state.annotation_data[user] = annotation_data
-
-
-def get_current_tag_index(annotation_data, index):
-    tag = annotation_data[index]
-    if tag in ["", "a", "b", "c", "d", "e"]:
-        return ["", "a", "b", "c", "d", "e"].index(tag)
+def get_selected_option(annotation_data, sentence):
+    annotation_row = annotation_data.loc[annotation_data["Sentence"] == sentence]
+    if not annotation_row.empty:
+        return annotation_row["Annotation"].values[0]
     return 0
 
+def update_annotation_data(annotation_data, sentence, tag):
+    annotation_row = annotation_data.loc[annotation_data["Sentence"] == sentence]
+    if not annotation_row.empty:
+        annotation_data.loc[annotation_row.index, "Annotation"] = tag
+    else:
+        annotation_data = annotation_data.append({"Sentence": sentence, "Annotation": tag}, ignore_index=True)
 
 if __name__ == "__main__":
     main()
