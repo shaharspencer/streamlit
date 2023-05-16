205a07e5b3b6d28dc4bed027e3ebba6b3dead912
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 18:12:38 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -4,46 +4,39 @@ import pandas as pd
 def main():
     # Create sidebar with two sections
     st.sidebar.title("Annotation App")
-    st.sidebar.header("Data File")
-    data_file = st.sidebar.file_uploader("Upload data file", type=["csv"])
-
     st.sidebar.header("Annotation Page")
     annotation_button = st.sidebar.button("Open Annotation Page")
 
-    if data_file is not None:
-        # Read the data file
-        data = pd.read_csv(data_file)
-
-        if annotation_button:
-            # Display annotation page
-            annotated_data = show_annotation_page(data)
-            show_annotated_data(annotated_data)
+    if annotation_button:
+        # Display annotation page
+        show_annotation_page()
 
 
-def show_annotation_page(data):
-    # Create an empty DataFrame to store annotations
-    annotated_data = pd.DataFrame(columns=["Sentence", "Annotation"])
+def show_annotation_page():
+    # Sample data and annotations
+    data = pd.DataFrame({
+        "Sentence": ["This is sentence 1", "This is sentence 2", "This is sentence 3"],
+    })
+    annotations = pd.DataFrame({
+        "Sentence": ["This is sentence 1", "This is sentence 2", "This is sentence 3"],
+        "Annotation": ["a", "b", "c"],
+    })
 
     # Display annotation page for each row in the data file
     for index, row in data.iterrows():
         sentence = row["Sentence"]
         st.write(f"**Sentence:** {sentence}")
 
+        # Get the annotation for the current sentence, if available
+        annotation_row = annotations.loc[annotations["Sentence"] == sentence]
+        selected_option = annotation_row["Annotation"].values[0] if not annotation_row.empty else None
+
         # Create a choice selection for each row
-        selected_option = st.selectbox("Choose an option", options=["a", "b", "c", "d"])
+        selected_option = st.selectbox("Choose an option", options=["a", "b", "c", "d"], index=selected_option)
 
         # Perform annotation task here (e.g., save selected_option to a database)
-        annotated_data = annotated_data.append({"Sentence": sentence, "Annotation": selected_option}, ignore_index=True)
 
         st.write("---")  # Add a separator between rows
-
-    return annotated_data
-
-
-def show_annotated_data(data):
-    # Display annotated data file
-    st.title("Annotated Data File")
-    st.write(data)
 
 
 if __name__ == "__main__":
