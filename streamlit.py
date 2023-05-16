124d1ae6740c05aad8b2307d05b45c046ad90320
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-16 21:36:49 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -20,19 +20,24 @@ def load_annotations(user):
     merged["Annotation"].fillna("a", inplace=True)  # Set default annotation to "a"
     return merged
 
-# Get user name
-user = st.text_input("Enter your name")
+# Main app
+def main():
+    # Get user name
+    user = st.sidebar.selectbox("Select User", ["Gabi", "Shahar", "Nurit", "Ittamar"])
 
-# Load user's annotations
-user_annotations = load_annotations(user)
+    # Load user's annotations
+    user_annotations = load_annotations(user)
 
-# Display user's annotations
-for index, row in user_annotations.iterrows():
-    st.write(row["Sentence"])
-    annotation = st.selectbox("Annotation", options=["a", "b", "c"], key=f"{user}_tag_selectbox_{index}", index=ord(row["Annotation"]) - ord("a"))
-    user_annotations.at[index, "Annotation"] = annotation
+    # Display user's annotations
+    for index, row in user_annotations.iterrows():
+        st.write(row["Sentence"])
+        annotation = st.selectbox("Annotation", choices=["a", "b", "c"], key=f"{user}_tag_selectbox_{index}", index=ord(row["Annotation"]) - ord("a"))
+        user_annotations.at[index, "Annotation"] = annotation
 
-    # Save user's annotations when changes are made
-    save_annotations(user, user_annotations)
+        # Save user's annotations when changes are made
+        save_annotations(user, user_annotations)
 
-st.write("Changes saved automatically")
+    st.write("Changes saved automatically")
+
+if __name__ == "__main__":
+    main()
