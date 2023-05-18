2f525b9cc478b447719f4351ba892d0624b07782
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 15:09:03 +0300

    maybe with option to download all dataframes

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import base64
 
 # Load data from CSV file
 data = pd.read_csv("your_dataframe.csv")
@@ -21,18 +20,9 @@ def load_annotations(user):
         annotations = pd.DataFrame(columns=["Sentence", "Annotation"])
     merged = pd.merge(data, annotations[["Sentence", "Annotation"]],
                       on="Sentence", how="outer")
-    merged["Annotation"].fillna("a",
-                                inplace=True)  # Set default annotation to "a"
+    merged["Annotation"].fillna("a", inplace=True)  # Set default annotation to "a"
     merged.drop_duplicates(inplace=True)  # Remove duplicate columns
     return merged
-
-
-# Define a function to download the dataframe as a CSV file
-def download_dataframe(dataframe):
-    csv = dataframe.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="user_annotations.csv">Download CSV</a>'
-    return href
 
 
 # Main app
@@ -43,49 +33,13 @@ def main():
     # Check if the "View Annotations" option is selected
     if st.sidebar.button("View All Annotations"):
         st.header("View All Annotations")
-        all_annotations = pd.DataFrame()  # Create an empty dataframe to store all annotations
         for user in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
             annotations = load_annotations(user)
             st.subheader(f"{user}'s Annotations")
-            st.dataframe(annotations)
-
-            # Add download button for the user's dataframe
-            st.markdown(download_dataframe(annotations), unsafe_allow_html=True)
-
-            # Concatenate the annotations to the all_annotations dataframe
-            all_annotations = pd.concat([all_annotations, annotations], ignore_index=True)
-
-        # Add a subheader for concatenated data
-        st.subheader("Concatenated Annotations")
-        st.dataframe(all_annotations)
-
-        # Add download button for the concatenated dataframe
-        st.markdown(download_dataframe(all_annotations), unsafe_allow_html=True)
-
-    # User Annotations page
-    st.sidebar.markdown("---")
-    st.sidebar.markdown("**User Annotations**")
-    user = st.sidebar.selectbox("Select User",
-                                ["Gabi", "Shahar", "Nurit", "Ittamar"])
-
-    # Load user's annotations
-    user_annotations = load_annotations(user)
-
-    # Display user's annotations
-    st.header(f"{user}'s Annotations")
-    for index, row in user_annotations.iterrows():
-        st.write(row["Sentence"])
-        annotation = st.selectbox("Annotation", options=["a", "b", "c"],
-                                  key=f"{user}_tag_selectbox_{index}",
-                                  index=ord(row["Annotation"]) - ord("a"))
-        user_annotations.at[index, "Annotation"] = annotation
-        # Save user's annotations for each selection
-        save_annotations(user, user_annotations)
-
-    st.write("Changes saved automatically")
-
-    # Add download button for the user's dataframe
-    st.markdown(download_dataframe(user_annotations), unsafe_allow_html=True)
+            if not annotations.empty:
+                st.dataframe(annotations)
+            else:
+                st.write("No annotations found for this user.")
 
 
 if __name__ == "__main__":
