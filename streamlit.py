f06af6a587c786581945336c4e38431e9b097404
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 15:23:52 +0300

    try

--- streamlit.py
+++ streamlit.py
@@ -19,10 +19,13 @@ def load_annotations(user):
         annotations = pd.read_csv(file_name)
     except FileNotFoundError:
         annotations = pd.DataFrame(columns=["Sentence", "Annotation"])
+
+    if "Annotation" not in annotations.columns:
+        annotations["Annotation"] = "a"  # Set default annotation to "a"
+
     merged = pd.merge(data, annotations[["Sentence", "Annotation"]],
                       on="Sentence", how="outer")
-    merged["Annotation"].fillna("a",
-                                inplace=True)  # Set default annotation to "a"
+    merged["Annotation"].fillna("a", inplace=True)  # Set default annotation to "a"
     merged.drop_duplicates(inplace=True)  # Remove duplicate columns
     return merged
 
@@ -41,37 +44,39 @@ def main():
     st.sidebar.markdown("**Annotations**")
 
     # Check if the "View Annotations" option is selected
-    if st.sidebar.button("View All Annotations"):
-        st.header("View All Annotations")
-        for user in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
-            annotations = load_annotations(user)
-            st.subheader(f"{user}'s Annotations")
-            st.dataframe(annotations)
+    view_all_annotations = st.sidebar.button("View All Annotations")
 
     # User Annotations page
     st.sidebar.markdown("---")
     st.sidebar.markdown("**User Annotations**")
-    user = st.sidebar.selectbox("Select User",
-                                ["Gabi", "Shahar", "Nurit", "Ittamar"])
+    user = st.sidebar.selectbox("Select User", ["Gabi", "Shahar", "Nurit", "Ittamar"])
 
-    # Load user's annotations
-    user_annotations = load_annotations(user)
+    if not view_all_annotations:
+        # Load user's annotations
+        user_annotations = load_annotations(user)
 
-    # Display user's annotations
-    st.header(f"{user}'s Annotations")
-    for index, row in user_annotations.iterrows():
-        st.write(row["Sentence"])
-        annotation = st.selectbox("Annotation", options=["a", "b", "c"],
-                                  key=f"{user}_tag_selectbox_{index}",
-                                  index=ord(row["Annotation"]) - ord("a"))
-        user_annotations.at[index, "Annotation"] = annotation
-        # Save user's annotations for each selection
-        save_annotations(user, user_annotations)
+        # Display user's annotations
+        st.header(f"{user}'s Annotations")
+        for index, row in user_annotations.iterrows():
+            st.write(row["Sentence"])
+            annotation = st.selectbox("Annotation", options=["a", "b", "c"],
+                                      key=f"{user}_tag_selectbox_{index}",
+                                      index=ord(row["Annotation"]) - ord("a"))
+            user_annotations.at[index, "Annotation"] = annotation
+            # Save user's annotations for each selection
+            save_annotations(user, user_annotations)
 
-    st.write("Changes saved automatically")
+        st.write("Changes saved automatically")
 
-    # Add download button for the user's dataframe
-    st.markdown(download_dataframe(user_annotations), unsafe_allow_html=True)
+        # Add download button for the user's dataframe
+        st.markdown(download_dataframe(user_annotations), unsafe_allow_html=True)
+
+    # View All Annotations page
+    st.header("View All Annotations")
+    for u in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
+        annotations = load_annotations(u)
+        st.subheader(f"{u}'s Annotations")
+        st.dataframe(annotations)
 
 
 if __name__ == "__main__":
