fe267a242304422b5d19a45345951dfdbc29921d
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 15:10:31 +0300

    back to prev version

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import base64
 
 # Load data from CSV file
 data = pd.read_csv("your_dataframe.csv")
@@ -20,9 +21,18 @@ def load_annotations(user):
         annotations = pd.DataFrame(columns=["Sentence", "Annotation"])
     merged = pd.merge(data, annotations[["Sentence", "Annotation"]],
                       on="Sentence", how="outer")
-    merged["Annotation"].fillna("a", inplace=True)  # Set default annotation to "a"
+    merged["Annotation"].fillna("a",
+                                inplace=True)  # Set default annotation to "a"
     merged.drop_duplicates(inplace=True)  # Remove duplicate columns
     return merged
+
+
+# Define a function to download the dataframe as a CSV file
+def download_dataframe(dataframe):
+    csv = dataframe.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="user_annotations.csv">Download CSV</a>'
+    return href
 
 
 # Main app
@@ -36,10 +46,32 @@ def main():
         for user in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
             annotations = load_annotations(user)
             st.subheader(f"{user}'s Annotations")
-            if not annotations.empty:
-                st.dataframe(annotations)
-            else:
-                st.write("No annotations found for this user.")
+            st.dataframe(annotations)
+
+    # User Annotations page
+    st.sidebar.markdown("---")
+    st.sidebar.markdown("**User Annotations**")
+    user = st.sidebar.selectbox("Select User",
+                                ["Gabi", "Shahar", "Nurit", "Ittamar"])
+
+    # Load user's annotations
+    user_annotations = load_annotations(user)
+
+    # Display user's annotations
+    st.header(f"{user}'s Annotations")
+    for index, row in user_annotations.iterrows():
+        st.write(row["Sentence"])
+        annotation = st.selectbox("Annotation", options=["a", "b", "c"],
+                                  key=f"{user}_tag_selectbox_{index}",
+                                  index=ord(row["Annotation"]) - ord("a"))
+        user_annotations.at[index, "Annotation"] = annotation
+        # Save user's annotations for each selection
+        save_annotations(user, user_annotations)
+
+    st.write("Changes saved automatically")
+
+    # Add download button for the user's dataframe
+    st.markdown(download_dataframe(user_annotations), unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
