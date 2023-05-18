f43e4fe421bebed3e6108af946807ffbb319b57f
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 14:36:40 +0300

    hi

--- streamlit.py
+++ streamlit.py
@@ -5,10 +5,12 @@ import base64
 # Load data from CSV file
 data = pd.read_csv("your_dataframe.csv")
 
+
 # Define a function to save annotations
 def save_annotations(user, annotations):
     file_name = f"{user}_annotations.csv"
     annotations.to_csv(file_name, index=False)
+
 
 # Define a function to load annotations
 def load_annotations(user):
@@ -17,10 +19,13 @@ def load_annotations(user):
         annotations = pd.read_csv(file_name)
     except FileNotFoundError:
         annotations = pd.DataFrame(columns=["Sentence", "Annotation"])
-    merged = pd.merge(data, annotations[["Sentence", "Annotation"]], on="Sentence", how="outer")
-    merged["Annotation"].fillna("a", inplace=True)  # Set default annotation to "a"
+    merged = pd.merge(data, annotations[["Sentence", "Annotation"]],
+                      on="Sentence", how="outer")
+    merged["Annotation"].fillna("a",
+                                inplace=True)  # Set default annotation to "a"
     merged.drop_duplicates(inplace=True)  # Remove duplicate columns
     return merged
+
 
 # Define a function to download the dataframe as a CSV file
 def download_dataframe(dataframe):
@@ -29,16 +34,15 @@ def download_dataframe(dataframe):
     href = f'<a href="data:file/csv;base64,{b64}" download="user_annotations.csv">Download CSV</a>'
     return href
 
+
 # Main app
 def main():
     # Sidebar menu
-    st.sidebar.image("view_annotations_icon.png", width=50)  # Replace with the path to your icon image
-    st.sidebar.markdown("**View Annotations**", unsafe_allow_html=True)
-    st.sidebar.markdown("---")
+    st.sidebar.markdown("**Annotations**")
 
-    # Check if the "View Annotations" page is selected
-    if st.sidebar.button("View Annotations"):
-        st.header("View Annotations")
+    # Check if the "View Annotations" option is selected
+    if st.sidebar.button("View All Annotations"):
+        st.header("View All Annotations")
         for user in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
             annotations = load_annotations(user)
             st.subheader(user)
@@ -47,7 +51,8 @@ def main():
     # User Annotations page
     st.sidebar.markdown("---")
     st.sidebar.markdown("**User Annotations**")
-    user = st.sidebar.selectbox("Select User", ["Gabi", "Shahar", "Nurit", "Ittamar"])
+    user = st.sidebar.selectbox("Select User",
+                                ["Gabi", "Shahar", "Nurit", "Ittamar"])
 
     # Load user's annotations
     user_annotations = load_annotations(user)
@@ -56,7 +61,9 @@ def main():
     st.header("User Annotations")
     for index, row in user_annotations.iterrows():
         st.write(row["Sentence"])
-        annotation = st.selectbox("Annotation", options=["a", "b", "c"], key=f"{user}_tag_selectbox_{index}", index=ord(row["Annotation"]) - ord("a"))
+        annotation = st.selectbox("Annotation", options=["a", "b", "c"],
+                                  key=f"{user}_tag_selectbox_{index}",
+                                  index=ord(row["Annotation"]) - ord("a"))
         user_annotations.at[index, "Annotation"] = annotation
         # Save user's annotations for each selection
         save_annotations(user, user_annotations)
@@ -65,6 +72,7 @@ def main():
 
     # Add download button for the user's dataframe
     st.markdown(download_dataframe(user_annotations), unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
