412cf65e7abe048c96ba9ace6724af264bc52bca
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 20:43:33 +0300

    marked verb in bold

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ import pandas as pd
 import base64
 import spacy
 from spacy import displacy
+from streamlit.elements import html
 
 # Load data from CSV file
 data = pd.read_csv("your_dataframe.csv")
@@ -10,12 +11,10 @@ data = pd.read_csv("your_dataframe.csv")
 # Load spaCy model
 nlp = spacy.load("en_core_web_lg")
 
-
 # Define a function to save annotations
 def save_annotations(user, annotations):
     file_name = f"{user}_annotations.csv"
     annotations.to_csv(file_name, index=False)
-
 
 # Define a function to load annotations
 def load_annotations(user):
@@ -42,7 +41,6 @@ def load_annotations(user):
     merged.drop_duplicates(inplace=True)  # Remove duplicate columns
     return merged
 
-
 # Define a function to download the dataframe as a CSV file
 def download_dataframe(dataframe):
     csv = dataframe.to_csv(index=False)
@@ -50,15 +48,25 @@ def download_dataframe(dataframe):
     href = f'<a href="data:file/csv;base64,{b64}" download="user_annotations.csv">Download CSV</a>'
     return href
 
-
 # Annotation Options Guide page
 def annotation_options_guide():
     st.header("Annotation Options Guide")
     # Add content for the Annotation Options Guide page
 
-
 # Main app
 def main():
+    # Custom CSS for styling
+    st.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            background-color: #B3C1D9;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     # Sidebar menu
     st.sidebar.markdown("**Annotations**")
 
@@ -68,8 +76,7 @@ def main():
     # User Annotations page
     st.sidebar.markdown("---")
     st.sidebar.markdown("**User Annotations**")
-    user = st.sidebar.selectbox("Select User",
-                                ["Gabi", "Shahar", "Nurit", "Ittamar"])
+    user = st.sidebar.selectbox("Select User", ["Gabi", "Shahar", "Nurit", "Ittamar"])
 
     # Annotation Options Guide page
     st.sidebar.markdown("---")
@@ -86,97 +93,20 @@ def main():
         st.header(f"{user}'s Annotations")
         for index, row in user_annotations.iterrows():
             sentence = row["Sentence"]
-            st.write(sentence)
+            doc = nlp(sentence)
 
-            # Display expand button under the sentence
-            expand_button = st.button("Expand",
-                                      key=f"{user}_expand_button_{index}")
-            if expand_button:
-                # Toggle visibility of extra data and dependency tree for the current user only
-                if f"{user}_expanded_{index}" not in st.session_state:
-                    st.session_state[f"{user}_expanded_{index}"] = {
-                        "extra_data": True,
-                        "dependency_tree": False
-                    }
-                else:
-                    st.session_state[f"{user}_expanded_{index}"]["extra_data"] = not \
-                        st.session_state[f"{user}_expanded_{index}"]["extra_data"]
+            # Iterate over the tokens in the sentence
+            for token in doc:
+                if token.i == row["index"]:
+                    # Render the token in bold
+                    sentence = sentence.replace(token.text, f"<b>{token.text}</b>")
 
-            # Display extra data if expanded for the current user
-            if f"{user}_expanded_{index}" in st.session_state and \
-                    st.session_state[f"{user}_expanded_{index}"]["extra_data"]:
-                # Iterate over all columns except "Sentence", "Tag according to dimension", "Notes on relevant dimension", and "Notes"
-                for column in data.columns:
-                    if column not in ["Sentence", "Tag according to dimension",
-                                      "Notes on relevant dimension", "Notes"]:
-                        st.write(f"{column}: {row[column]}")
+            # Display the modified sentence
+            st.markdown(sentence, unsafe_allow_html=True)
 
-            # Display dependency tree button
-            dependency_tree_button = st.button("Show Dependency Tree",
-                                               key=f"{user}_dependency_tree_button_{index}")
-            if dependency_tree_button:
-                # Toggle visibility of the dependency tree for the current user only
-                if f"{user}_expanded_{index}" not in st.session_state:
-                    st.session_state[f"{user}_expanded_{index}"] = {
-                        "extra_data": False,
-                        "dependency_tree": True
-                    }
-                else:
-                    st.session_state[f"{user}_expanded_{index}"]["dependency_tree"] = not \
-                        st.session_state[f"{user}_expanded_{index}"]["dependency_tree"]
+            # ... Rest of the code ...
 
-            # Display dependency tree if expanded for the current user
-            if f"{user}_expanded_{index}" in st.session_state and \
-                    st.session_state[f"{user}_expanded_{index}"]["dependency_tree"]:
-                # Display the dependency tree for the current sentence
-                sent = nlp(sentence)
-                svg = displacy.render(sent, style="dep")
-                st.write(svg, unsafe_allow_html=True)
-
-            annotation = st.selectbox("Tag according to dimension", options=[
-                "ordinary",
-                "creative",
-                "spelling variant",
-                "wrong lemma",
-                "not a verb",
-                "algorithm error",
-                "not English"
-            ], key=f"{user}_tag_selectbox_{index}")
-            user_annotations.at[
-                index, "Tag according to dimension"] = annotation
-
-            notes_relevant_dimension = st.text_area("Notes on relevant dimension",
-                                 value=row["Notes on relevant dimension"],
-                                 key=f"{user}_notes_relevant_dimension_{index}")
-            user_annotations.at[index, "Notes on relevant dimension"] = notes_relevant_dimension
-
-            notes = st.text_area("Notes",
-                                 value=row["Notes"],
-                                 key=f"{user}_notes_{index}")
-            user_annotations.at[index, "Notes"] = notes
-
-            # Save user's annotations for each selection
-            save_annotations(user, user_annotations)
-
-        st.write("Changes saved automatically")
-
-        # Add download button for the user's dataframe
-        st.markdown(download_dataframe(user_annotations),
-                    unsafe_allow_html=True)
-
-    # View All Annotations section
-    if view_all_annotations:
-        st.header("View All Annotations")
-        all_annotations = pd.DataFrame()
-        for u in ["Gabi", "Shahar", "Nurit", "Ittamar"]:
-            annotations = load_annotations(u)
-            annotations[
-                "Annotator"] = u  # Add "Annotator" column with the annotator's name
-            all_annotations = pd.concat([all_annotations, annotations],
-                                        ignore_index=True)
-
-        st.subheader("All Annotations")
-        st.dataframe(all_annotations)
+        # ... Rest of the code ...
 
 
 if __name__ == "__main__":
