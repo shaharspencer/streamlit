e8c5e972de9678d639f3d254827503bf8be3df0f
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-18 20:52:33 +0300

    marked verb in bold

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,6 @@ import base64
 import spacy
 from spacy import displacy
 
-
 # Load data from CSV file
 data = pd.read_csv("your_dataframe.csv")
 
@@ -104,10 +103,107 @@ def main():
             # Display the modified sentence
             st.markdown(sentence, unsafe_allow_html=True)
 
-            # ... Rest of the code ...
+            # Display expand button under the sentence
+            expand_button = st.button("Expand", key=f"{user}_expand_button_{index}")
+            if expand_button:
+                # Toggle visibility of extra data and dependency tree for the current user only
+                if f"{user}_expanded_{index}" not in st.session_state:
+                    st.session_state[f"{user}_expanded_{index}"] = {
+                        "extra_data": True,
+                        "dependency_tree": False
+                    }
+                else:
+                    st.session_state[f"{user}_expanded_{index}"]["extra_data"] = not \
+                        st.session_state[f"{user}_expanded_{index}"]["extra_data"]
 
-        # ... Rest of the code ...
+            # Display extra data if expanded for the current user
+            if f"{user}_expanded_{index}" in st.session_state and \
+                    st.session_state[f"{user}_expanded_{index}"]["extra_data"]:
+                # Iterate over all columns except "Sentence", "Tag according to dimension", "Notes on relevant dimension", and "Notes"
+                for column in data.columns:
+                    if column not in ["Sentence", "Tag according to dimension",
+                                      "Notes on relevant dimension", "Notes"]:
+                        st.write(f"{column}: {row[column]}")
 
+            # Display dependency tree button
+            dependency_tree_button = st.button("Show Dependency Tree",
+                                               key=f"{user}_dependency_tree_button_{index}")
+            if dependency_tree_button:
+                # Toggle visibility of the dependency tree for the current user only
+                if f"{user}_expanded_{index}" not in st.session_state:
+                    st.session_state[f"{user}_expanded_{index}"] = {
+                        "extra_data": False,
+                        "dependency_tree": True
+                    }
+                else:
+                    st.session_state[f"{user}_expanded_{index}"]["dependency_tree"] = not \
+                        st.session_state[f"{user}_expanded_{index}"]["dependency_tree"]
 
+            # Display dependency tree if expanded for the current user
+            if f"{user}_expanded_{index}" in st.session_state and \
+                    st.session_state[f"{user}_expanded_{index}"]["dependency_tree"]:
+                # Display the dependency tree for the current sentence
+                sent = nlp(sentence)
+                svg = displacy.render(sent, style="dep")
+                st.write(svg, unsafe_allow_html=True)
+
+            annotation = st.selectbox("Tag according to dimension", options=[
+                "ordinary",
+                "creative",
+                "spelling variant",
+                "wrong lemma",
+                "not a verb",
+                "algorithm error",
+                "not English"
+            ], key=f"{user}_tag_selectbox_{index}")
+            user_annotations.at[index, "Tag according to dimension"] = annotation
+
+            notes_relevant_dimension = st.text_area("Notes on relevant dimension",
+                                                    value=row["Notes on relevant dimension"],
+                                                    key=f"{user}_notes_relevant_dimension_{index}")
+            user_annotations.at[index, "Notes on relevant dimension"] = notes_relevant_dimension
+
+            notes = st.text_area("Notes",
+                                 value=row["Notes"],
+                                 key=f"{user}_notes_{index}")
+            user_annotations.at[index, "Notes"] = notes
+
+            # Save user's annotations for each selection
+            save_annotations(user, user_annotations)
+
+        st.write("Changes saved automatically")
+
+        # Add download button for the user's dataframe
+        st.markdown(download_dataframe(user_annotations),
+                    unsafe_allow_html=True)
+
+    # View All Annotations section
+    if view_all_annotations:
+        st.header("All Annotations")
+        for index, row in data.iterrows():
+            sentence = row["Sentence"]
+            doc = nlp(sentence)
+
+            # Iterate over the tokens in the sentence
+            for token in doc:
+                if token.i == row["index of verb"]:
+                    # Render the token in bold
+                    sentence = sentence.replace(token.text, f"<b>{token.text}</b>")
+
+            # Display the modified sentence
+            st.markdown(sentence, unsafe_allow_html=True)
+
+            # Display all columns except "Sentence", "Tag according to dimension", "Notes on relevant dimension", and "Notes"
+            for column in data.columns:
+                if column not in ["Sentence", "Tag according to dimension",
+                                  "Notes on relevant dimension", "Notes"]:
+                    st.write(f"{column}: {row[column]}")
+
+    # Footer
+    st.sidebar.markdown("---")
+    st.sidebar.markdown("**About**")
+    st.sidebar.write("This app allows users to annotate sentences according to dimensions.")
+
+# Run the app
 if __name__ == "__main__":
     main()
