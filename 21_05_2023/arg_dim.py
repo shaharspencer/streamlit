5e9898f22d86424c4111fabcdcd90b09731ad044
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-21 19:01:51 +0300

    fix render error

--- 21_05_2023/arg_dim.py
+++ 21_05_2023/arg_dim.py
@@ -105,7 +105,7 @@ def main():
             for token in doc:
                 if token.i == row["index of verb"]:
                     # Render the token in bold
-                    new_sentence = sentence.replace(token.text,
+                    new_sentence = sentence.copy().replace(token.text,
                                                 f"<b>{token.text}</b>")
             sentence_number = index + 1
             st.markdown(f"Sentence {sentence_number}: {new_sentence}", unsafe_allow_html=True)
