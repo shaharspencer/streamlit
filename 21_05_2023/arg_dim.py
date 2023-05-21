ad68c7922d2db173eca90e2ec2d5a49c396a71c3
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-05-21 18:13:30 +0300

    com

--- 21_05_2023/arg_dim.py
+++ 21_05_2023/arg_dim.py
@@ -6,7 +6,7 @@ from spacy import displacy
 
 
 # Load data from CSV file
-data = pd.read_csv("21_05_2023//morph_order_by_entropy_and_verb_perc2023_05_21.csv")
+data = pd.read_csv(r"21_05_2023//morph_order_by_entropy_and_verb_perc2023_05_21.csv")
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_lg")
